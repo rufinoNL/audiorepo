44371efcad0fb2bb544f856858e186985eb8e3bb
Jonathan Ramos <rufino@rufino.nl>
2017-05-07 18:42:39 +0200

    Refactor MainView

--- daw/src/main/java/nl/rufino/daw/view/MainView.java
+++ daw/src/main/java/nl/rufino/daw/view/MainView.java
@@ -2,6 +2,7 @@ package nl.rufino.daw.view;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,16 +25,20 @@ import nl.rufino.util.AudioFunctions;
 import nl.rufino.util.WindowsFunctions;
 
 public class MainView extends Application implements EventHandler<ActionEvent>{
+	
 	static final Logger LOGGER = LoggerFactory.getLogger(MainView.class);
 	
 //	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw_data");
 //	private EntityManager em = emf.createEntityManager();
 	
-	private Label lblText;
+	private Label lblMessages;
 	private Button btnStartCubaseReason;
 	//  Currently commented out because database offline
 	//private MainViewData data = new MainViewData(em);
 	private TableView<MusicEntity> musicTable;
+
+	private String audioApplicationReason;
+	private String audioApplicationCubase;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -44,12 +49,8 @@ public class MainView extends Application implements EventHandler<ActionEvent>{
 		//Set style
 		FlatterFX.style();
 		
-		lblText = new Label();
-		lblText.setWrapText(true);
-		btnStartCubaseReason = new Button();
-		btnStartCubaseReason.setText("Start Cubase and Reason in ReWire");
-		btnStartCubaseReason.setOnAction(this);
-		
+		setupView();
+
 		//Column for id
 		TableColumn<MusicEntity, Integer> musicIdColumn = new TableColumn<>("Id");
 		musicIdColumn.setMinWidth(15);
@@ -59,14 +60,10 @@ public class MainView extends Application implements EventHandler<ActionEvent>{
 		TableColumn<MusicEntity, Integer> trackNameColumn = new TableColumn<>("Track name");
 		trackNameColumn.setMinWidth(200);
 		trackNameColumn.setCellValueFactory(new PropertyValueFactory<>("trackName"));
-//      Currently commented out because database offline		
-		musicTable = new TableView<MusicEntity>();
-//		musicTable.setItems(data.getMusic());
-//		musicTable.getColumns().addAll(musicIdColumn, trackNameColumn);
+		configureTable();
 		
 		VBox root = new VBox();
-
-		root.getChildren().addAll(lblText, btnStartCubaseReason, musicTable);
+		root.getChildren().addAll(lblMessages, btnStartCubaseReason, musicTable);
 		
 		Scene scene = new Scene(root, 750, 1000);
 		stage.setScene(scene);
@@ -75,24 +72,52 @@ public class MainView extends Application implements EventHandler<ActionEvent>{
 		
 	@Override
 	public void handle(ActionEvent arg0) {
-		lblText.setText("Copying template and creating new folder to make epic beats...");
-		File[] fileToStart = AudioFunctions.createNewTemplate();
+		message("Copying template and creating new folder");
+		File[] filesToStart = AudioFunctions.createNewTemplate();
 		
 		try {
-			lblText.setText("Starting Cubase...");
-			WindowsFunctions.startApplication("C:\\Program Files\\Steinberg\\Cubase 6\\Cubase6.exe", 
-					fileToStart[0].toString());
-			//Wait 25 seconds for Reason to Start...
+			message("Starting Cubase...");
+			WindowsFunctions.startApplication(audioApplicationCubase, 
+					filesToStart[0].toString());
+			
 			Thread.sleep(25000);
-			lblText.setText("Starting Reason...");
-			WindowsFunctions.startApplication("C:\\Program Files\\Propellerhead\\Reason\\Reason.exe", 
-					fileToStart[1].toString());
+			
+			message("Starting Reason...");
+			WindowsFunctions.startApplication(audioApplicationReason, 
+					filesToStart[1].toString());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void setupView() {
+		configureLabelForMessages();
+		configureStartButton();
+	}
+
+	private void configureLabelForMessages() {
+		lblMessages = new Label();
+		lblMessages.setWrapText(true);
+	}
+
+	private void configureStartButton() {
+		btnStartCubaseReason = new Button();
+		btnStartCubaseReason.setText("Start Cubase and Reason in ReWire");
+		btnStartCubaseReason.setOnAction(this);
+	}
+	
+	private void configureTable() {
+		//Currently commented out because database offline		
+		musicTable = new TableView<MusicEntity>();
+		//musicTable.setItems(data.getMusic());
+		//musicTable.getColumns().addAll(musicIdColumn, trackNameColumn);
+	}
+
+	private void message(String message) {
+		lblMessages.setText(message);
 	}
 
 }
